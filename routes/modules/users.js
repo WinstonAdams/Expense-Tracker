897cf3b551598570b1ae8633d4843a0fdddaafdf
WinstonAdams <apollo07291@gmail.com>
2023-07-19 22:01:06 +0800

    feat: add login and register page

---
+++ routes/modules/users.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+const User = require('../../models/users')
+
+router.get('/login', (req, res) => {
+
+  res.render('login')
+})
+
+router.get('/register', (req, res) => {
+
+  res.render('register')
+})
+
+module.exports = router
