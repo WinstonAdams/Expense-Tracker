7c98c6eeba9191f05c6ba372e05aa9e495ed5c67
WinstonAdams <apollo07291@gmail.com>
2023-07-21 11:08:54 +0800

    feat: add new page

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const users = require('./modules/users')
 const auth = require('./modules/auth')
+const records = require('./modules/records')
 
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
 router.use('/auth', auth)
+router.use('/records', authenticator, records)
 router.use('/', authenticator, home)
 
 module.exports = router
